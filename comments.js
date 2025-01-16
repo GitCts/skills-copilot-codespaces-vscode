f0196a4a48d2d9934f5d1cb4001ef351f2f8017a
TBM <163563438+GitCts@users.noreply.github.com>
2025-01-16 15:22:29 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,47 +1,17 @@
-
 //create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var querystring = require('querystring');
-var comments = [];
+const express = require('express');
+const app = express();
+const port = 3000;
+//use the comments.js file
+const comments = require('./comments.js');
 
-// 1. create server
-http.createServer(function(req, res) {
-  //parse url
-  var urlObj = url.parse(req.url, true);
-  var pathName = urlObj.pathname;
-  if (pathName == '/') {
-    fs.readFile('./index.html', function(err, data) {
-      if (err) {
-        console.log(err);
-        res.end('read file error');
-      } else {
-        res.end(data);
-      }
-    });
-  } else if (pathName == '/addComment') {
-    var comment = urlObj.query;
-    comments.push(comment);
-    res.end(JSON.stringify(comments));
-  } else {
-    fs.readFile('.' + pathName, function(err, data) {
-      if (err) {
-        console.log(err);
-        res.end('read file error');
-      } else {
-        res.end(data);
-      }
-    });
-  }
-}).listen(3000, function() {
-  console.log('server is running...');
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//use the comments.js file
+app.use('/comments', comments);
+
+//listen on port 3000
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-// 2. create comments array
-// 3. add /addComment route, when user submit a comment, add it to comments array
-// 4. when user visit /loadComments, return comments array
-// 5. when user visit other routes, return 404
-// 6. when user visit /, return index.html
-// 7. when user visit /index.html, return index.html
-// 8. when user visit /style.css, return style.css
-// 9. when user visit /comments.js, return comments.js
